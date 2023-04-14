b61a04eafe24e86ee7ca747a413b7aacca353d77
cucuridas@gamil.com <3310223@naver.com>
2023-04-14 11:03:17 +0900

    프로젝트 생성 및 필수 서비스 3개 형틀 등록

---
+++ app/core/server.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.append("/Users/cucuridas/Desktop/chatbot_tg")
+sys.path.append("/home/cucuridas/chatbot")
+from typing import Union
+from fastapi import FastAPI
+from app.api.v1.healthCheck import router
+
+
+
+"""
+fastapi 서버를 실행 시키는 파일입니다
+"""
+
+
+def createApp() -> FastAPI:
+    fastApiServer = FastAPI()
+    
+    return fastApiServer
+
+
+app: FastAPI = createApp()
