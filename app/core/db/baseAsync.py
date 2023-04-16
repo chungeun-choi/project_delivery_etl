7d55927919b7a750b1d786acaac039a46094b41f
cucuridas@gamil.com <3310223@naver.com>
2023-04-16 11:54:31 +0900

    Database async session 로직 생성

--- app/core/db/baseAsync.py
+++ app/core/db/baseAsync.py
@@ -1,8 +1,14 @@
+from asyncio import current_task
 from app.core.config import Settings
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
-
+from sqlalchemy.ext.asyncio import (
+    async_scoped_session,
+    create_async_engine,
+    AsyncEngine,
+)
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from typing import Union
 
 
 """
@@ -14,15 +20,12 @@ sessionmaker 클래스를 통해 session 연결을 진행합니다
 SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{Settings.DB_USER}:{Settings.DB_PASS}@{Settings.DB_HOST}:{Settings.DB_PORT}/{Settings.DB_NAME}"
 
 engine: AsyncEngine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True)
-Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+session: Union[AsyncSession, async_scoped_session] = async_scoped_session(
+    session_factory=session_maker,
+    scopefunc=current_task,
+)
+
 
 Base = declarative_base()
-
-
-def get_db():
-    db = None
-    try:
-        db = Session()
-        yield db
-    finally:
-        db.close()
