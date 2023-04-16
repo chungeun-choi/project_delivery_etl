ff61bdda5f8933f5eb686bd900045edc6f054567
cucuridas@gamil.com <3310223@naver.com>
2023-04-16 11:10:45 +0900

    alembi을 통한 'connection_information' 테이블 생성 추가

--- app/core/config.py
+++ app/core/config.py
@@ -9,3 +9,10 @@ class Settings:
     SERVER_WORKER: int = os.getenv("SERVER_WORKER",1)
     SERVER_HOST: str = os.getenv("SERVER_HOST")
     SERVER_PORT: int = os.getenv("SERVER_PORT",8000)
+    
+    # Database connection information
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: int = os.getenv("DB_PORT")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASS: str = os.getenv("DB_PASS")
+    DB_NAME: str = os.getenv("DB_NAME")
---
+++ deliverty_etl/versions/d133155c3572_create_db_to_save_connection_information.py
@@ -0,0 +1,36 @@
+"""Create db to save connection information
+
+Revision ID: d133155c3572
+Revises: 
+Create Date: 2023-04-16 10:48:15.632477
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd133155c3572'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    connection_information_table = op.create_table(
+    "connceciont_info",
+        sa.Column("id", sa.BigInteger(), primary_key=True),
+        sa.Column("connection_name", sa.String(length=200), nullable=False, unique=True),
+        sa.Column("host", sa.String(length=200), nullable=False),
+        sa.Column("port", sa.Integer(), nullable=False),
+        sa.Column("user", sa.String(length=200), nullable=False),
+        sa.Column("password", sa.String(length=200), nullable=False),
+        sa.Column("header", sa.JSON(), nullable=False),
+        sa.Column("extra", sa.JSON(), nullable=False),
+
+
+    )
+
+def downgrade() -> None:
+    op.drop_table("connceciont_info")
+    
