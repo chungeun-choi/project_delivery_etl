0e8c7583adc430c6a3b8306eec5fe0c23e7d784d
cucuridas@gamil.com <3310223@naver.com>
2023-05-07 19:07:03 +0900

    띄어 쓰기 변환

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 import sys
+
 sys.path.append("/Users/cucuridas/Desktop/project_delivery_etl")
 import uvicorn
 from app.core.server import app
 from app.core.config import Settings
 
 
-
 def main():
-
     uvicorn.run(
         app="core.server:app",
         host=Settings.SERVER_HOST,
         port=Settings.SERVER_PORT,
         loop="auto",
-        #workers=2,
+        # workers=2,
     )
 
 
